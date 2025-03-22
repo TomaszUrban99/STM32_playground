0fe7ad89bc6380c62d9bf163f1a52a80f1b9c8f0
Tomasz Urban <tomasz.urban.116@gmail.com>
2025-03-22 22:45:50 +0100

    Setting up environment for developing software for B-L4S5I-IOT01A

---
+++ B-L4S5I-IOT01A/.metadata/.plugins/org.eclipse.core.resources/.history/ed/c07f32ce66070010131e9220c274f901
@@ -0,0 +1,5 @@
+#include <stm32l4s5xx.h>
+
+int main(void)
+{
+}
---
+++ B-L4S5I-IOT01A/HTS221/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Device/ST/STM32L4xx/Include/stm32l4s5xx.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/core_cm4.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_version.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_compiler.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_gcc.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/mpu_armv7.h \
+ /home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Device/ST/STM32L4xx/Include/stm32l4s5xx.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/core_cm4.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_version.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_compiler.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/cmsis_gcc.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Include/mpu_armv7.h:
+/home/tomasz/Documents/Projekty/STM32_Disc/STM32_playground/B-L4S5I-IOT01A/HTS221/cmsis/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
